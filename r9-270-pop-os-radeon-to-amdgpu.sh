b1de5192c684bfb2a3521c84f5fa160827372cb5
toni-dev <62844491+Toni-d-e-v@users.noreply.github.com>
2021-11-29 21:54:31 +0100

    Create r9-270-pop-os-radeon-to-amdgpu.sh

---
+++ r9-270-pop-os-radeon-to-amdgpu.sh
@@ -0,0 +1,4 @@
+sudo kernelstub --add-options radeon.cik_support=0
+sudo kernelstub --add-options amdgpu.cik_support=1
+sudo kernelstub --add-options radeon.si_support=0
+sudo kernelstub --add-options amdgpu.si_support=1
